a0f68d9bac4c0f278266faf2e730bb713dc71216
edilainerodrigues <edilaine.letras@gmaiil.com>
2024-07-13 18:02:46 -0300

    Update queue.js
    adiciona metodos

--- queue.js
+++ queue.js
@@ -1,10 +1,23 @@
 class Queue {
-  elements
   constructor(){
     this.elements = []
   }
-  
-  // Implementar aqui
+
+  size(){
+    return this.elements.length
+  }
+
+  add(element) {
+    return this.elements.push(element)
+  }
+
+  dequeue() {
+    if(this.size() > 0) return this.elements.shift()
+  }
+
+  peek() {
+    if(this.size() > 0) return this.elements[0]
+  }
 }
 
 module.exports = Queue
